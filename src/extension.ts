4d2c8f2479261e9514f169d844eb690061589fd9
Brandon Martin-Anderson <badhill@gmail.com>
2023-05-02 00:39:06 -0700

    Refactor extension.ts and fix linting issues
    - Extracted the main functionality from activate() into
      draftCommitMessage() to separate activation and primary functionality.
    - Added eslint-disable directive to avoid naming issues with snake_case
      in the first line.
    - Minor formatting and clean up to improve readability of the code.

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import * as vscode from "vscode";
 import { simpleGit, SimpleGit, CleanOptions } from "simple-git";
 import fetch from "node-fetch";
@@ -55,95 +56,95 @@ function appendToLastLine(editor: vscode.TextEditor, text: string) {
   });
 }
 
+async function draftCommitMessage() {
+  const workspaceRootPath = vscode.workspace.workspaceFolders?.[0].uri.path;
+  const git: SimpleGit = simpleGit(workspaceRootPath);
+
+  const isRepo = await git.checkIsRepo();
+  if (!isRepo) {
+    vscode.window.showErrorMessage(
+      `Current working directory is not a git repository`
+    );
+
+    return;
+  }
+
+  const diff = await git.diff();
+
+  const conf = vscode.workspace.getConfiguration("commitcomposer");
+  // get API key from settings
+  const openaiApiKey: string | undefined = conf.get("openaiApiKey");
+  if (!openaiApiKey) {
+    vscode.window.showErrorMessage(`OpenAI API key not set`);
+
+    return;
+  }
+
+  const modelName: string | undefined = conf.get("modelName");
+
+  // create a blank new next window
+  const commitMessageDoc = await vscode.workspace.openTextDocument({
+    content: "",
+    language: "plaintext",
+  });
+  const docEditor = await vscode.window.showTextDocument(commitMessageDoc, {
+    viewColumn: vscode.ViewColumn.Beside,
+    preserveFocus: true,
+    preview: false,
+  });
+
+  try {
+    const response = fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${openaiApiKey}`,
+      },
+      body: JSON.stringify({
+        model: modelName,
+        messages: [
+          { role: "system", content: SYSTEM_MESSAGE },
+          { role: "user", content: diff },
+        ],
+        stream: true,
+      }),
+    });
+
+    const reader = (await response).body?.setEncoding("utf-8");
+
+    if (!reader) {
+      vscode.window.showErrorMessage(`Error getting response body`);
+      return;
+    }
+
+    for await (const chunk of reader) {
+      // parse chunk, which is a server-sent event starting with
+      // "data: " and ending with "\n\n"
+      const lines = chunk.toString().split("\n\n");
+
+      const parsedLines = lines
+        .map((line) => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix
+        .filter((line) => line !== "" && line !== "[DONE]") // Remove empty lines and "[DONE]"
+        .map((line) => JSON.parse(line) as ChatCompletionResponse); // Parse the JSON string
+
+      for (const line of parsedLines) {
+        const word = line.choices[0].delta.content;
+        if (!word) {
+          continue;
+        }
+
+        appendToLastLine(docEditor, word);
+      }
+    }
+  } catch (error) {
+    vscode.window.showErrorMessage(`Error generating commit message: ${error}`);
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "commitcomposer.draftCommitMessage",
-    async () => {
-      const workspaceRootPath = vscode.workspace.workspaceFolders?.[0].uri.path;
-      const git: SimpleGit = simpleGit(workspaceRootPath);
-
-      const isRepo = await git.checkIsRepo();
-      if (!isRepo) {
-        vscode.window.showErrorMessage(
-          `Current working directory is not a git repository`
-        );
-
-        return;
-      }
-
-      const diff = await git.diff();
-
-      const conf = vscode.workspace.getConfiguration("commitcomposer");
-      // get API key from settings
-      const openaiApiKey: string | undefined = conf.get("openaiApiKey");
-      if (!openaiApiKey) {
-        vscode.window.showErrorMessage(`OpenAI API key not set`);
-
-        return;
-      }
-
-      const modelName: string | undefined = conf.get("modelName");
-
-      // create a blank new next window
-      const commitMessageDoc = await vscode.workspace.openTextDocument({
-        content: "",
-        language: "plaintext",
-      });
-      const docEditor = await vscode.window.showTextDocument(commitMessageDoc, {
-        viewColumn: vscode.ViewColumn.Beside,
-        preserveFocus: true,
-        preview: false,
-      });
-
-      try {
-        const response = fetch(API_URL, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${openaiApiKey}`,
-          },
-          body: JSON.stringify({
-            model: modelName,
-            messages: [
-              { role: "system", content: SYSTEM_MESSAGE },
-              { role: "user", content: diff },
-            ],
-            stream: true,
-          }),
-        });
-
-        const reader = (await response).body?.setEncoding("utf-8");
-
-        if (!reader) {
-          vscode.window.showErrorMessage(`Error getting response body`);
-          return;
-        }
-
-        for await (const chunk of reader) {
-          // parse chunk, which is a server-sent event starting with
-          // "data: " and ending with "\n\n"
-          const lines = chunk.toString().split("\n\n");
-
-          const parsedLines = lines
-            .map((line) => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix
-            .filter((line) => line !== "" && line !== "[DONE]") // Remove empty lines and "[DONE]"
-            .map((line) => JSON.parse(line) as ChatCompletionResponse); // Parse the JSON string
-
-          for (const line of parsedLines) {
-            const word = line.choices[0].delta.content;
-            if (!word) {
-              continue;
-            }
-
-            appendToLastLine(docEditor, word);
-          }
-        }
-      } catch (error) {
-        vscode.window.showErrorMessage(
-          `Error generating commit message: ${error}`
-        );
-      }
-    }
+    draftCommitMessage
   );
 
   context.subscriptions.push(disposable);
